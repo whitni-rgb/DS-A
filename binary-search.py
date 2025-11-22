788494aec8f5f6fd2be704148c8e88b3bfdf8276
Whitni McElreath <whitni@Whitnis-MacBook-Pro.local>
2025-11-21 17:43:45 -0700

    final binary search

--- binary-search.py
+++ binary-search.py
@@ -9,31 +9,34 @@ Binary search Big-O = logN
 Search algorithm to find target of SORTED list.
 Works by determining if current guess is higher or lower than the target and eliminates that side of the branch.
 
+Will find the index of the target whether the target is in the array itself or not,
+ to see where the index would be you can return low
+
 Sidenote: 
 
 Should use
-mid = (low + (high - low)) // 2
+mid = low + (high - low) // 2
 instead of 
-mid = ((high + low)- 1) // 2
+mid = (high + low) // 2
 
 - Will prevent integer overflow for languages that specifically have a memory limit.
 - Faster
- 
-
-Fixed index, need to figure out the comparison here use while low <= high 
 '''
 
 "Returns the index of the target" 
 def binarySearch(numbers, target):
     low = 0
     high = len(numbers) - 1
-    mid = (low + (high - low)) // 2
-    print(low)
-    print(high)
-    print(mid)
-
     while low <= high:
+        mid = low + ((high - low) // 2)
+        guess = numbers[mid]
         if target == numbers[mid]:
-            print("found")
+            print(mid)
+            break
+        elif guess < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+    print(low)
 
-binarySearch(other, 2)
+binarySearch(sortedList, 17)
